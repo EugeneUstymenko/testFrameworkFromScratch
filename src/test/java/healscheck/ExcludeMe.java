59df784b6873ac3e46d6e754714821b1cfd6fe45
Evgenii Ustimenko <u.evgenii7@gmail.com>
2022-08-02 00:31:56 +0300

    Initial commits

---
+++ src/test/java/healscheck/ExcludeMe.java
@@ -0,0 +1,14 @@
+package healscheck;
+
+import org.testng.annotations.Test;
+
+public class ExcludeMe {
+    @Test
+    public void excludeMeTest(){
+        System.out.println("Exclude me");
+    }
+    @Test
+    public void excsludeMeTest(){
+        System.out.println("Exclude me");
+    }
+}
